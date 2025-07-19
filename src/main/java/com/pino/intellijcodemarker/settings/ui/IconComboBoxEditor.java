4399be1e66ae90a4ac06c97b737aac10ea077e4a
Pino <pino.chang@cathayholdings.com.tw>
2025-07-20 00:35:13 +0800

    feat: enhance IconComboBoxEditor to disable automatic selection and improve editing behavior

--- src/main/java/com/pino/intellijcodemarker/settings/ui/IconComboBoxEditor.java
+++ src/main/java/com/pino/intellijcodemarker/settings/ui/IconComboBoxEditor.java
@@ -3,6 +3,7 @@ package com.pino.intellijcodemarker.settings.ui;
 import com.intellij.openapi.ui.ComboBox;
 
 import javax.swing.*;
+import java.awt.*;
 
 @SuppressWarnings("unchecked")
 public class IconComboBoxEditor extends DefaultCellEditor {
@@ -14,8 +15,20 @@ public class IconComboBoxEditor extends DefaultCellEditor {
         this.comboBox = (ComboBox<String>) getComponent();
         this.comboBox.setRenderer(new IconListCellRenderer());
 
-        // Set click count to 1 for immediate editing
+        // Set click count to 1 for normal editing behavior
         setClickCountToStart(1);
+        
+        // Disable automatic selection of first item when popup opens
+        comboBox.putClientProperty("JComboBox.isTableCellEditor", Boolean.TRUE);
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        // Set the current value
+        comboBox.setSelectedItem(value);
+        
+        // Return the comboBox component
+        return comboBox;
     }
 
     @Override
